ff11873b6e0ec9842bcd45246ce2245851461a54
orioncrocker <orioncrocker@gmail.com>
2018-04-22 19:59:32 -0700

    unfinished tile scripts

---
+++ Assets/scripts/TileReplacement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// I have no idea what I'm doing - Kleiner
+
+public class TileReplacement : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject replacement;
+
+	void Start()
+    {
+
+	}
+
+    void Update()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        //TileReplacementMenu.instance.ShowMenu(this);
+        InstantiateTile();
+    }
+
+    public void InstantiateTile(TileSelectMenu.TileItem ti)
+    {
+        Debug.Log(name + " Game Object Clicked!");
+        Tile t = GameObject.Instantiate<Tile>(ti.tile);
+        t.transform.position = gameObject.transform.position;
+        GameObject.Destroy(this.gameObject);
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        // When selected tile is clicked, course becomes yellow
+        gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+}
---
+++ Assets/scripts/TileSelectMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileSelectMenu : MonoBehaviour
+{
+    [System.Serializable]
+	public struct TileItem
+	{
+        public string name;
+        public int cost;
+        public Tile tile;
+	}
+
+    public TileItem hillItem;
+
+    public Button hillButton;
+
+    public static TileSelectMenu instance;
+
+    private TileReplacement _curPlacement;
+
+    void Start()
+    {
+        instance = this;
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ShowMenu(TileReplacement replacement)
+    {
+        gameObject.SetActive(true);
+        hillButton.GetComponentIntChildren<Text>().text = hillItem.name + " $" + hillItem.cost;
+    }
+
+    public void BuyHillTile()
+    {
+        if (_curPlacement != null)
+        {
+            GolfCourse.instance.SetMoney(Golfcourse.instance.curMoney - hillItem.cost);
+            _curPlacement.InstantiateTower(hillItem);
+        }
+        this.gameObject.SetActive(false);
+    }
+}
