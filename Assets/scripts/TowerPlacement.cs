f8d6fac03a1f8359155653a01530f1fc6c8e3ceb
Kevin Kohler <kkohler@hitpointstudios.com>
2018-04-22 20:44:30 -0400

    Added .obj files

--- Assets/scripts/TowerPlacement.cs
+++ Assets/scripts/TowerPlacement.cs
@@ -6,20 +6,17 @@ using UnityEngine.EventSystems;
 public class TowerPlacement : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject aimTarget;
-
-    // Use this for initialization
+    
     void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
 
     public void OnPointerClick(PointerEventData pointerEventData)
     {
-        //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
         TowerSelectMenu.instance.ShowMenu(this);
     }
 
